4fc0dd0b1bf5705f382439474fa6333e755686cc
Parth Patel <creatinganewway@gmail.com>
2026-01-16 14:34:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/instagram' , (req , res)=>{
+    res.send("_parthkesath_")
+})
+
+app.get('/login' , (req , res)=>{
+    res.send('<h1>LOGIN KAR LE LADLEEE</h1>')
+})
+
+app.listen(process.env.PORT , () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
